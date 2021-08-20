373d0a303ac313eb388ded30d1524dbc4ba57636
Sebastian Schaffer <26623931+sschaffer92@users.noreply.github.com>
2021-08-20 18:44:45 +0200

    Add basic test suite

--- test/YakStrategy.spec.js
+++ test/YakStrategy.spec.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const fs = require("fs");
 const { ethers, network, deployments } = require("hardhat");
 const { accounts, accountsFixture, tokens, pairs, stakingRewards } = require("./fixtures");
-const { fundLiquidityToken, getTokenContract, getPairContract, oneWeek } = require("./utils");
+const { fundLiquidityToken, fundWAVAX, fundToken, getTokenContract, getPairContract, oneWeek, getWAVAXContract } = require("./utils");
 
 const BigNumber = ethers.BigNumber
 const ONE_EXP_17 = BigNumber.from("10000000000000000");
@@ -18,14 +18,132 @@ describe("DexStrategyV6", function () {
         ({ owner, deployer, alice, bob } = await accountsFixture())
     })
 
+    describe("Test attributes", async () => {
+        let dexStrategyV6 = await deployStrategy(
+            {
+                minTokenToReinvest: ONE_EXP_17,
+                adminFee: 100,
+                devFee: 100,
+                reinvestRewardBips: 500
+            }
+        )
+
+        it('Owner is set', async () => {
+            expect(await dexStrategyV6.owner()).to.equal(owner.address)
+        })
+
+        it('Set correctly the MIN_TOKENS_TO_REINVEST', async () => {
+            expect(await dexStrategyV6.MIN_TOKENS_TO_REINVEST()).to.eq(ONE_EXP_17)
+        })
+
+        it('Set correctly the ADMIN_FEE_BIPS', async () => {
+            expect(await dexStrategyV6.ADMIN_FEE_BIPS()).to.eq(100)
+        })
+
+        it('Set correctly the DEV_FEE_BIPS', async () => {
+            expect(await dexStrategyV6.DEV_FEE_BIPS()).to.eq(100)
+        })
+
+        it('Set correctly the REINVEST_REWARD_BIPS', async () => {
+            expect(await dexStrategyV6.REINVEST_REWARD_BIPS()).to.eq(500)
+        })
+
+        it('Set correctly the devAddr', async () => {
+            expect(await dexStrategyV6.devAddr()).to.equal(deployer.address)
+        })
+
+        it('Set correctly the allowance on the deposit token', async () => {
+            const depositToken = await getTokenContract(pairs.AVAX.PNG)
+            expect(await depositToken.allowance(dexStrategyV6.address, stakingRewards.PGL.AVAX.PNG)).to.eq(ethers.constants.MaxUint256)
+        })
+
+        it('DEPOSITS_ENABLED is true', async () => {
+            expect(await dexStrategyV6.DEPOSITS_ENABLES()).to.be.true
+        })
+
+        it("MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST is zero", async () => {
+            expect(await dexStrategyV6.MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST())
+                .to.be.eq(0)
+        })
+
+    })
+
+    describe("Test Owner functionallity", async () => {
+        let dexStrategyV6;        
+        beforeEach(async () => {
+            dexStrategyV6 = await deployStrategy(
+                {
+                    owner: owner.address
+                }
+            )
+            dexStrategyV6 = dexStrategyV6.connect(owner)
+        })
+
+        it("revoke allowance", async () => {
+            await dexStrategyV6.revokeAllowance(pairs.AVAX.PNG, stakingRewards.PGL.AVAX.PNG)
+            let depositToken = await getTokenContract(pairs.AVAX.PNG)
+            expect(await depositToken.allowance(dexStrategyV6.address, stakingRewards.PGL.AVAX.PNG)).to.eq(0)
+            await depositToken.setAllowance
+        })
+
+        it("Set MIN_TOKENS_TO_REINVEST", async () =>  {
+            await dexStrategyV6.updateMinTokensToReinvest("1000")
+            expect(await dexStrategyV6.MIN_TOKENS_TO_REINVEST()).to.be.eq("1000")
+        })
+
+        it("Set MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST", async () => {
+            await dexStrategyV6.updateMaxTokensToDepositWithoutReinvest("1000")
+            expect(await dexStrategyV6.MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST()).to.be.eq("1000")
+        })
+
+        it("Set DEV_FEE_BIPS", async () => {
+            await dexStrategyV6.updateDevFee("200")
+            expect(await dexStrategyV6.DEV_FEE_BIPS()).to.be.eq("200")
+        })
+
+        it("Set ADMIN_FEE_BIPS", async () => {
+            await dexStrategyV6.updateAdminFee("200")
+            expect(await dexStrategyV6.ADMIN_FEE_BIPS()).to.be.eq("200")
+        })
+
+        it("Set REINVEST_REWARD_BIPS", async () => {
+            await dexStrategyV6.updateReinvestReward("500")
+            expect(await dexStrategyV6.REINVEST_REWARD_BIPS()).to.be.eq("500")
+        })
+
+        it("Set DEPOSITS_ENABLED", async () => {
+            await dexStrategyV6.updateDepositsEnabled(false)
+            expect(await dexStrategyV6.DEPOSITS_ENABLED()).to.be.false
+        })
+
+        it("set devAddr", async () => {
+            await dexStrategyV6.connect(deployer).updateDevAddr(alice.address)
+            expect(await dexStrategyV6.devAddr()).to.be.equal(alice.address)
+        })
+
+        it("recoverERC20 can recover deposit pair", async () => {
+            const amount = ethers.utils.parseEther("100")
+            const WAVAXContract = (await getWAVAXContract()).connect(alice)
+            await fundWAVAX(alice, amount)
+            await WAVAXContract.transfer(dexStrategyV6.address, amount)
+            expect(await WAVAXContract.balanceOf(dexStrategyV6.address)).to.be.gt(0)
+            await dexStrategyV6.recoverERC20(WAVAXContract.address, amount)
+            expect(await WAVAXContract.balanceOf(dexStrategyV6.address)).to.eq(0)
+            expect(await WAVAXContract.balanceOf(owner.address)).to.be.eq(amount)
+        })
+
+    })
+
+    it("Deposit fails if DEPOSITS_ENABLED is false", async () => {})
+
     it("Can deploy", async () => {
-        const dexStrategyV6 = await deployV6()
+        const dexStrategyV6 = await deployStrategy()
         expect(await dexStrategyV6.stakingContract()).to.be.equal(stakingRewards.PGL.AVAX.PNG)
         expect(await dexStrategyV6.owner()).to.be.equal(owner.address)
     })
 
     it("test deposit", async () => {
-        const dexStrategyV6 = await deployV6()
+        const dexStrategyV6 = await deployStrategy()
         const farm = dexStrategyV6.connect(alice)
         const liquidityToken = await getTokenContract(pairs.AVAX.PNG, alice)
         const liquidityBalance = await fundLiquidityToken(alice, liquidityToken.address, ethers.utils.parseEther("10"))
@@ -42,13 +160,13 @@ describe("DexStrategyV6", function () {
 
 
     it("test deposit with unclaimed rewards", async () => {
-        const dexStrategyV6 = await deployV6({reinvestRewardBips: 500})
-        const farm = dexStrategyV6.connect(alice)        
+        const dexStrategyV6 = await deployStrategy({ reinvestRewardBips: 500 })
+        const farm = dexStrategyV6.connect(alice)
         await fundStrategy(bob, farm, ethers.utils.parseEther("100"))
         const totalDepositsBefore = await farm.totalDeposits()
-        
+
         await oneWeek()
-        
+
         await farm.connect(owner)
             .updateMaxTokensToDepositWithoutReinvest(ONE_EXP_17)
         const maxTokensToDepositWithoutReinvest = await farm.MAX_TOKENS_TO_DEPOSIT_WITHOUT_REINVEST()
@@ -56,10 +174,10 @@ describe("DexStrategyV6", function () {
 
         const rewardsBefore = await farm.checkReward()
         expect(rewardsBefore).to.be.gt(maxTokensToDepositWithoutReinvest)
-        
+
         // second deposit triggers reinvest
         const alicesDeposits = await fundStrategy(alice, farm, ethers.utils.parseEther("10"))
-        
+
         const totalDepositsAfter = await farm.totalDeposits()
         // total deposits increased by staking rewards
         expect(totalDepositsAfter).to.be.gt(totalDepositsBefore.add(alicesDeposits))
@@ -67,9 +185,9 @@ describe("DexStrategyV6", function () {
 
 
     it("test withdraw", async () => {
-        const dexStrategyV6 = await deployV6()
+        const dexStrategyV6 = await deployStrategy()
         const farm = dexStrategyV6.connect(alice)
-        
+
         const deposits = await fundStrategy(alice, farm, ethers.utils.parseEther("10"))
 
         await oneWeek()
@@ -81,8 +199,8 @@ describe("DexStrategyV6", function () {
     })
 
     it("test reinvest", async () => {
-        const minTokenToReinvest = ethers.utils.parseUnits("1.0", 15)
-        const dexStrategyV6 = await deployV6({reinvestRewardBips: 500, minTokenToReinvest})
+        const minTokenToReinvest = ethers.utils.parseUnits("1.0", 14)
+        const dexStrategyV6 = await deployStrategy({ reinvestRewardBips: 500, minTokenToReinvest })
         const pngToken = await getTokenContract(tokens.PNG, alice)
         const farm = dexStrategyV6.connect(alice)
         const bobsDeposits = await fundStrategy(bob, farm, ethers.utils.parseEther("100"))
@@ -96,22 +214,22 @@ describe("DexStrategyV6", function () {
         expect(await pngToken.balanceOf(alice.address)).to.be.eq(0)
         // reinvesting is possible
         expect(await farm.checkReward()).to.be.gt(minTokenToReinvest)
-        
+
         await farm.reinvest()
-        
+
         const totalDepositsAfter = await farm.totalDeposits()
         // check reinvestment
         expect(totalDepositsAfter).to.be.gt(totalDepositsBefore)
         expect(await farm.checkReward()).to.lt(minTokenToReinvest)
         // some rewards went to alice
         expect(await pngToken.balanceOf(alice.address)).to.be.gt(0)
-
     })
 })
 
 
-async function deployV6(
-    { 
+async function deployStrategy(
+    {
+        strategy = "DexStrategyV6",
         name = "TestContract",
         depositToken = pairs.AVAX.PNG,
         rewardToken = tokens.PNG,
@@ -123,11 +241,13 @@ async function deployV6(
         adminFee = 0,
         devFee = 0,
         reinvestRewardBips = 0,
-    }={}) {
-    const dexStrategyV6Factory = await ethers.getContractFactory("DexStrategyV6")
-    if (owner === null) owner = (await accountsFixture()).owner.address
+    } = {}) {
+    const { owner: _owner, deployer } = await accountsFixture()
+    const strategyFactory = await ethers.getContractFactory(strategy, deployer)
 
-    dexStrategyV6 = await dexStrategyV6Factory.deploy(
+    if (owner === null) owner = _owner.address
+
+    const _strategy = await strategyFactory.deploy(
         name,
         depositToken, // deposit token
         rewardToken, // reward token
@@ -140,15 +260,15 @@ async function deployV6(
         devFee, // dev fee
         reinvestRewardBips // reinvest reward bips
     )
-    return dexStrategyV6
+    return _strategy
 }
 
-async function fundStrategy(signer, strategy, amount=ethers.utils.parseEther("10")) {
+async function fundStrategy(signer, strategy, amount = ethers.utils.parseEther("10")) {
     const depositToken = await strategy.depositToken()
-    const liquidityToken = await getTokenContract(depositToken, signer)
-    const liquidityBalance = await fundLiquidityToken(signer, liquidityToken.address, amount)
+    const depositTokenContract = (await (await getTokenContract(depositToken)).connect(signer)
+    const liquidity = await fundLiquidityToken(signer, depositTokenContract.address, amount)
     const depositsBefore = await strategy.balanceOf(signer.address)
-    await liquidityToken.approve(strategy.address, liquidityBalance)
-    await strategy.connect(signer).deposit(liquidityBalance)
+    await depositTokenContract.approve(strategy.address, liquidity)
+    await strategy.connect(signer).deposit(liquidity)
     return (await strategy.balanceOf(signer.address)).sub(depositsBefore)
 }
